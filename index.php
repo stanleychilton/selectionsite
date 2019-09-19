218e02bcf503d2a312cda97423a089b35ab8ab60
stanleys desktop\Stanley Chilton <stanleychilton@live.com>
2019-09-19 17:32:08 +1200

    updating the selection divs
    fixed css now it floats the button to the right hand side and also has the border around each instance. also adding distance between the columns

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $conn    = Connect();
 								echo"<div class='row round3'><div id='sides' class='col-md-6'>";
 								echo"instance-".($i+1)."</div>";
 								echo"<div id='sides' class='col-md-6'><button class='button1'>select</button>";
-								echo"</div></div>";
+								echo"</div></div><br>";
 							}
 
 						?>
