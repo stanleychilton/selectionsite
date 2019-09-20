4c1fb4017f5d90249f6b616158c7495546bb8f04
stanleys desktop\Stanley Chilton <stanleychilton@live.com>
2019-09-20 12:03:00 +1200

    Update selection.php
    updating to a working version of the system (no lockout yet)

--- selection.php
+++ selection.php
@@ -8,7 +8,23 @@ $id = $_GET['id'];
 
 echo $email . $id;
 
+$sql = "UPDATE students SET selected=1 WHERE email='$email'";
 
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$sql = "UPDATE instances SET active=1 WHERE id=$id";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+header('Location: /selectionsite');
 
 
 
