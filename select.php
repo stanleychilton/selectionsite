f4a16094c6af8e0db897039b207789f416c9e30f
stan surface <stanleychilton@live.com>
2019-09-26 16:31:53 +1200

    updating the count type

--- select.php
+++ select.php
@@ -35,17 +35,21 @@ $email = $_POST['email'];
 					<p>Email: <?php echo $email; ?>
 					<br>
 						<?php
-						$sql = "SELECT id, active FROM instances WHERE active != 1";
+						$sql = "SELECT id, active FROM instances";
 						$result = $conn->query($sql);
 
 						if ($result->num_rows > 0) {
 							// output data of each row
 							while($row = $result->fetch_assoc()) {
-								echo"<div class='row round3'><div id='sides' class='col-md-6'>";
-								echo"instance-".$row['id']."</div>";
-								echo"<div id='sides' class='col-md-6'>";
-								echo"<a href='selection.php?id=".$row['id']."&email=".$email."' class='button' style='text-decoration:none;'>Select</a>";
-								echo"</div></div><br>";
+								$count = 1;
+								if(active != 1){
+									echo"<div class='row round3'><div id='sides' class='col-md-6'>";
+									echo"instance-".$count."</div>";
+									echo"<div id='sides' class='col-md-6'>";
+									echo"<a href='selection.php?id=".$row['id']."&email=".$email."' class='button' style='text-decoration:none;'>Select</a>";
+									echo"</div></div><br>";
+								}
+								$count ++;
 							}
 						}
 
