0260012aeb01f91212b6a0f1065ef9ad7b3c9330
stanleys desktop\Stanley Chilton <stanleychilton@live.com>
2019-10-01 20:54:16 +1300

    Update test.py
    added a if else to check for dates so either all data or specific data is retieved and added comments for jacobs

--- test.py
+++ test.py
@@ -8,9 +8,16 @@ test = []
 test.append(input1)
 test.append(input2)
 
+if test[0] == Null or test[1] == Null:
+	## script here to grab all data from the cost software
+else:
+	## code here to grab specific date data 
+	## test[0] is start date
+	## test[1] is end date
+	
 string  = ""
 for i in test:
 	string += i
-	string += "<br>"
+	string += "<br>" ## formatting needs to be done here
 	
-print(string)
+print(string)  ## passes the string data back to the php script
