a458ce9c37a51284d2e8abfdbcd75f773c01458f
stanley chilton <stanleychilton@live.com>
2019-10-06 18:49:34 +1300

    Update specificstatus.py

--- specificstatus.py
+++ specificstatus.py
@@ -1,56 +1,16 @@
-#!/usr/bin/env python3
+import boto3, sys
 
-import boto3
-import datetime
-import re
+string  = "<table>"
+ec2 = boto3.resource('ec2')
 
+input1 = sys.argv[1]
 
+for instance in ec2.instances.all():
+    if str(instance.id) == input1:
+        string += "<tr>"
+        string += str(instance.id)  
+        string += "          " + instance.state['Name']
+        string += "</tr>"
+string += "</table>"   
 
-
-
-    # Create a Cost Explorer client
-client = boto3.client('ce')
-
-    # Set time range to cover the last full calendar month
-    # Note that the end date is EXCLUSIVE (e.g., not counted)
-now = datetime.datetime.utcnow()
-    # Set the end of the range to start of the current month
-end = datetime.datetime(year=now.year, month=now.month, day=1)
-    # Subtract a day and then "truncate" to the start of previous month
-start = end - datetime.timedelta(days=1)
-start = datetime.datetime(year=start.year, month=start.month, day=1)
-    # Get the month as string for email purposes
-month = start.strftime('%Y-%m')
-
-    # Convert them to strings
-start = start.strftime('%Y-%m-%d')
-end = end.strftime('%Y-%m-%d')
-
-
-response = client.get_cost_and_usage(
-        TimePeriod={
-            'Start': start,
-            'End':  end
-        },
-        Granularity='MONTHLY',
-        Metrics=['BlendedCost'],
-        GroupBy=[
-            {
-                'Type': 'TAG',
-                'Key': 'Project'
-            },
-        ]
-    )
-
-    #pprint.pprint()
-
-tsv_lines = []
-for project in response["ResultsByTime"][0]["Groups"]:
-        namestring = project['Keys'][0]
-        
-
-        amount = project['Metrics']['BlendedCost']['Amount']
-        amount = float(amount)
-        line = "${:,.2f}".format(amount)
-        print("The Predicted cost of your account next month is " + line)
-        tsv_lines.append(line)
+print(string)
