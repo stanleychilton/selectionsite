79c4e55edca0c6dee54d3127043c8b2c8b2dabb2
Rahtz <47646398+Rahtz@users.noreply.github.com>
2019-10-06 18:01:29 +1300

    Update index.php

--- index.php
+++ index.php
@@ -1,177 +1,43 @@
-<!--
-//index.php
-!-->
-
 <?php
-
-include('database_connection.php');
-
-session_start();
-
-if(!isset($_SESSION['user_id']))
-{
- header("location:login.php");
-}
-
+require 'connect.php';
+$conn    = Connect();
+include('session.php');
 ?>
-
-<html>  
-    <head>  
-        <title>Chat Application using PHP Ajax Jquery</title>  
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.rawgit.com/mervick/emojionearea/master/dist/emojionearea.min.css">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="https://cdn.rawgit.com/mervick/emojionearea/master/dist/emojionearea.min.js"></script>
-    </head>  
-    <body>  
-        <div class="container">
-   <br />
-   
-   <h3 align="center">Chat Application using PHP Ajax Jquery</a></h3><br />
-   <br />
-   
-   <div class="table-responsive">
-    <h4 align="center">Online User</h4>
-    <p align="right">Hi - <?php echo $_SESSION['username'];  ?> - <a href="logout.php">Logout</a></p>
-    <div id="user_details"></div>
-    <div id="user_model_details"></div>
-   </div>
-  </div>
-    </body>  
-</html>  
-
-
-
-
-<script>  
-$(document).ready(function(){
-
- fetch_user();
-
- setInterval(function(){
-  update_last_activity();
-  fetch_user();
-  update_chat_history_data();
- }, 5000);
-
- function fetch_user()
- {
-  $.ajax({
-   url:"fetch_user.php",
-   method:"POST",
-   success:function(data){
-    $('#user_details').html(data);
-   }
-  })
- }
-
- function update_last_activity()
- {
-  $.ajax({
-   url:"update_last_activity.php",
-   success:function()
-   {
-
-   }
-  })
- }
-
- function make_chat_dialog_box(to_user_id, to_user_name)
- {
-  var modal_content = '<div id="user_dialog_'+to_user_id+'" class="user_dialog" title="You have chat with '+to_user_name+'">';
-  modal_content += '<div style="height:400px; border:1px solid #ccc; overflow-y: scroll; margin-bottom:24px; padding:16px;" class="chat_history" data-touserid="'+to_user_id+'" id="chat_history_'+to_user_id+'">';
-  modal_content += fetch_user_chat_history(to_user_id);
-  modal_content += '</div>';
-  modal_content += '<div class="form-group">';
-  modal_content += '<textarea name="chat_message_'+to_user_id+'" id="chat_message_'+to_user_id+'" class="form-control chat_message"></textarea>';
-  modal_content += '</div><div class="form-group" align="right">';
-  modal_content+= '<button type="button" name="send_chat" id="'+to_user_id+'" class="btn btn-info send_chat">Send</button></div></div>';
-  $('#user_model_details').html(modal_content);
- }
-
- $(document).on('click', '.start_chat', function(){
-  var to_user_id = $(this).data('touserid');
-  var to_user_name = $(this).data('tousername');
-  make_chat_dialog_box(to_user_id, to_user_name);
-  $("#user_dialog_"+to_user_id).dialog({
-   autoOpen:false,
-   width:400
-  });
-  $('#user_dialog_'+to_user_id).dialog('open');
-  $('#chat_message_'+to_user_id).emojioneArea({
-   pickerPosition:"top",
-   toneStyle: "bullet"
-  });
- });
-
- $(document).on('click', '.send_chat', function(){
-  var to_user_id = $(this).attr('id');
-  var chat_message = $('#chat_message_'+to_user_id).val();
-  $.ajax({
-   url:"insert_chat.php",
-   method:"POST",
-   data:{to_user_id:to_user_id, chat_message:chat_message},
-   success:function(data)
-   {
-    //$('#chat_message_'+to_user_id).val('');
-    var element = $('#chat_message_'+to_user_id).emojioneArea();
-    element[0].emojioneArea.setText('');
-    $('#chat_history_'+to_user_id).html(data);
-   }
-  })
- });
-
- function fetch_user_chat_history(to_user_id)
- {
-  $.ajax({
-   url:"fetch_user_chat_history.php",
-   method:"POST",
-   data:{to_user_id:to_user_id},
-   success:function(data){
-    $('#chat_history_'+to_user_id).html(data);
-   }
-  })
- }
-
- function update_chat_history_data()
- {
-  $('.chat_history').each(function(){
-   var to_user_id = $(this).data('touserid');
-   fetch_user_chat_history(to_user_id);
-  });
- }
-
- $(document).on('click', '.ui-button-icon', function(){
-  $('.user_dialog').dialog('destroy').remove();
- });
-
- $(document).on('focus', '.chat_message', function(){
-  var is_type = 'yes';
-  $.ajax({
-   url:"update_is_type_status.php",
-   method:"POST",
-   data:{is_type:is_type},
-   success:function()
-   {
-
-   }
-  })
- });
-
- $(document).on('blur', '.chat_message', function(){
-  var is_type = 'no';
-  $.ajax({
-   url:"update_is_type_status.php",
-   method:"POST",
-   data:{is_type:is_type},
-   success:function()
-   {
+<!DOCTYPE html>
+<html lang="en" style = 'padding-right: 0px;'>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+		<link rel="stylesheet" href="css/index.css"> 
+    </head>
+    <body style = 'padding-right: 0px;'>
+		
+        <div id="mainfield">
+            <div class="row" style="margin-right:0px;margin-left:0px">
+			
+				<div id='sides' class="col-md-3">
+				
+				</div>
+                <div id='main' class="col-md-6 col-md-offset-0" style = 'padding-right: 0px;padding-left: 0px;'>
+					<div class="contain">
+						<form action="select.php" method="post">
+							Student ID: <input type="text" name="sid">
+							<input type="submit" name="submit" value="Submit">
+						</form>
+					</div>
+				
+				
+                </div>
+				
+				<div id='sides' class="col-md-3 col-md-offset" style="overflow:hidden;">
+				
+				
+				</div>
+			</div>
     
-   }
-  })
- });
- 
-});  
-</script>
+	
+		</div>
+    </body>
+</html>
